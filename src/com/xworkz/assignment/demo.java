9d17f357a4254297c547a0627459888f78cac4b0
rohan <rohanprasad1434@gmail.com>
2025-04-02 00:01:30 +0530

    initial commit

---
+++ src/com/xworkz/assignment/demo.java
@@ -0,0 +1,12 @@
+package com.xworkz.assignment;
+
+public class demo {
+    String name;
+}
+class demo1 {
+    public static void main(String[] args) {
+        demo mo = new demo();
+        mo.name = "Rony";
+        System.out.println("Hello World" + mo.name);
+    }
+}
