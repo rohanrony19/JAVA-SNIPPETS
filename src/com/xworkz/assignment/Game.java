97c51132061f62b9b0c9973cbd948fd31c0093c0
rohan <rohanprasad1434@gmail.com>
2025-04-02 00:13:39 +0530

    initial commit

---
+++ src/com/xworkz/assignment/Game.java
@@ -0,0 +1,25 @@
+package com.xworkz.assignment;
+
+public class Game {
+
+    public static void main(String[] values){
+            System.out.println("to store reference of Game");
+            String[] game = {"Sekiro: Shadows Die Twice", "Dark Souls", "Dark Souls II", "Dark Souls III", "Demon's Souls",
+                    "Bloodborne", "The Witcher 3: Wild Hunt", "Red Dead Redemption 2", "Cyberpunk 2077",
+                    "Grand Theft Auto V", "Minecraft", "League of Legends", "Counter-Strike: Global Offensive",
+                    "Dota 2", "Apex Legends", "Fortnite", "Valorant", "Call of Duty: Warzone",
+                    "The Elder Scrolls V: Skyrim", "The Sims 4", "World of Warcraft", "Overwatch",
+                    "Rocket League", "Dark Souls III", "Rainbow Six Siege", "Horizon Zero Dawn", "Far Cry 5",
+                    "Assassin's Creed Valhalla", "Tomb Raider", "DOOM Eternal", "Civilization VI", "Bioshock Infinite",
+                    "Half-Life: Alyx", "Gears 5", "The Last of Us Part II", "Control", "Hitman 3", "Death Stranding",
+                    "Ghost of Tsushima", "Subnautica", "Stardew Valley", "Hunt: Showdown", "Sea of Thieves",
+                    "Battlefield V", "PUBG: Battlegrounds", "Final Fantasy XIV", "Monster Hunter: World",
+                    "Fallout 4", "Satisfactor","Satisfactory", "Valhallah", "Metro Exodus", "Call of Duty: Black Ops Cold War",
+                    "Fall Guys", "Wasteland 3", "Planet Coaster", "Total War: Three Kingdoms", "XCOM 2",
+                    "Kerbal Space Program", "Mordhau", "Factorio"};
+            for(String games: game){
+                System.out.println(games);
+            }
+        }
+    }
+
