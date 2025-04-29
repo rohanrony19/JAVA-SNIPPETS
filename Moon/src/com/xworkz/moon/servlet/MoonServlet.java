6a5c9e5e51fb882a967e6796628d2abb16c62186
Rohan <rohanprasad1434@gmail.com>
2025-04-29 18:16:26 +0530

    commit servlets

--- Moon/src/com/xworkz/moon/servlet/MoonServlet.java
+++ Moon/src/com/xworkz/moon/servlet/MoonServlet.java
@@ -1,4 +1,21 @@
 package com.xworkz.moon.servlet;
 
-public class MoonServlet {
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/moon",loadOnStartup = 1)
+public class MoonServlet extends GenericServlet {
+
+    public MoonServlet(){
+        System.out.println("MoonServlet created by Tomcat");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Running service");
+    }
 }
