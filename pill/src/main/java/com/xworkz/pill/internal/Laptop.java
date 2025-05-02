c95bcfbb620e945a16f4fde30a334f15acc3620b
Rohan <rohanprasad1434@gmail.com>
2025-05-02 22:54:30 +0530

    xlsx commit

---
+++ pill/src/main/java/com/xworkz/pill/internal/Laptop.java
@@ -0,0 +1,42 @@
+package com.xworkz.pill.internal;
+
+import org.apache.poi.xssf.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Laptop {
+
+    public  void excelRead() throws IOException {
+
+        String path="C:\\Users\\rohan\\OneDrive\\Documents\\JAVA-SNIPPETS\\JAVA-SNIPPETS\\pill\\src\\main\\resources\\sample_excel.xlsx";
+        FileInputStream inputStream=new FileInputStream(path);
+
+        XSSFWorkbook workbook=new XSSFWorkbook(inputStream);
+        XSSFSheet sheet= workbook.getSheet("sheet1");
+
+        int rows= sheet.getLastRowNum();
+        int cols=sheet.getRow(1).getLastCellNum();
+
+        for(int r=0; r<rows; r++){
+           XSSFRow row =sheet.getRow(r);
+            for (int c=0; c<cols;c++){
+                XSSFCell cell= row.getCell(c);
+
+                switch (cell.getCellType()){
+                    case STRING:
+                        System.out.println(cell.getStringCellValue() +"/t");break;
+                    case NUMERIC:
+                        System.out.println(cell.getNumericCellValue()+"/t");break;
+                }
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
+}
---
+++ pill/src/main/java/com/xworkz/pill/runner/MainRunner.java
@@ -0,0 +1,12 @@
+package com.xworkz.pill.runner;
+
+import com.xworkz.pill.internal.Laptop;
+
+import java.io.IOException;
+
+public class MainRunner {
+    public static void main(String[] args) throws IOException {
+        Laptop laptop=new Laptop();
+        laptop.excelRead();
+    }
+}
