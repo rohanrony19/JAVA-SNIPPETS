bac82f4aa3294bf5ee98feabd5b1a52080c1171d
Rohan <rohanprasad1434@gmail.com>
2025-05-05 23:48:48 +0530

    Basic commit

---
+++ Basic DSA/com/xworkz/Basic/Hello.java
@@ -0,0 +1,13 @@
+package com.xworkz.Basic;
+
+import java.util.Scanner;
+
+public class Hello {
+     public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String name= sc.nextLine();
+            System.out.println( "Hello " +name);
+        }
+    }
+
+
