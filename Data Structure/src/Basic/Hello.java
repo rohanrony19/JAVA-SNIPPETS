182aaea4eadf0d5ee51e046f0b795efc447a29ee
Rohan <rohanprasad1434@gmail.com>
2025-05-06 00:05:20 +0530

    new commit

--- Data Structure/src/Basic/Hello.java
+++ Data Structure/src/Basic/Hello.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String name=sc.nextLine();
+        System.out.print("Enter your name:");
+        String name=sc.nextLine();//Whole line (includes spaces)
+        //String name= sc.next(); 	One word (stops at space)
         System.out.println("Hello " + name);
     }
 }
