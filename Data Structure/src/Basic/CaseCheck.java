6e9d385e6885d430669332f584ed80ce7ff23865
Rohan <rohanprasad1434@gmail.com>
2025-05-06 21:00:36 +0530

    new commit

---
+++ Data Structure/src/Basic/CaseCheck.java
@@ -0,0 +1,11 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().trim().charAt(2);
+        System.out.println(ch);
+    }
+}
