b9ce77a710eab494e687c0f300afd8866f2abcd9
Rohan <rohanprasad1434@gmail.com>
2025-05-07 23:32:53 +0530

    commit method

---
+++ Data Structure/src/Basic/Methods/StringExample.java
@@ -0,0 +1,20 @@
+package Basic.Methods;
+
+public class StringExample {
+
+    public static void main(String[] args) {
+        String message = dominos();
+        System.out.println(message);
+    }
+    static String dominos(){
+        String pizza = "pizza is delicious ";
+        return pizza;
+    }
+
+    /*
+    return_type name(arguments) {
+        //body
+        return statement;
+    }
+     */
+}
