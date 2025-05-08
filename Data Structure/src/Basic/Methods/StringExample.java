0628ea928028972ed39f5c9ce818a4faf1eb5222
Rohan <rohanprasad1434@gmail.com>
2025-05-08 22:28:38 +0530

    commit method

--- Data Structure/src/Basic/Methods/StringExample.java
+++ Data Structure/src/Basic/Methods/StringExample.java
@@ -3,8 +3,15 @@ package Basic.Methods;
 public class StringExample {
 
     public static void main(String[] args) {
-        String message = dominos();
-        System.out.println(message); 
+//        String message = dominos();
+//        System.out.println(message);
+
+        String personalized = myGreat("Rony");
+        System.out.println(personalized);
+    }
+    static  String myGreat(String name){
+        String message = "Hello " + name;
+        return message;
     }
     static String dominos(){
         String pizza = "pizza is delicious ";
