59fb7d8b1e895e0bbb29c60cb8ae08c0659a437d
Rohan <rohanprasad1434@gmail.com>
2025-05-09 00:19:01 +0530

    commit method

--- Data Structure/src/Basic/Methods/ChangeValue.java
+++ Data Structure/src/Basic/Methods/ChangeValue.java
@@ -6,7 +6,7 @@ public class ChangeValue {
     public static void main(String[] args) {
         // create an array
         int[] arr = {1,3,2,45,6};
-        change(arr); 
+        change(arr);
         System.out.println(Arrays.toString(arr));
     }
 
