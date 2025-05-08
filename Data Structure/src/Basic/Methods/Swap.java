da24879ecf636f6938321e935426897976dfe746
Rohan <rohanprasad1434@gmail.com>
2025-05-08 22:38:42 +0530

    commit method

--- Data Structure/src/Basic/Methods/Swap.java
+++ Data Structure/src/Basic/Methods/Swap.java
@@ -6,12 +6,24 @@ public class Swap {
         int b = 20;
 
         //swap numbers code
+//        int temp = a;
+//        a = b;
+//        b = temp;
+        swap(10,20);
+        System.out.println(a + " " + b);
+
+        String name = "rony";
+        changeName(name);
+        System.out.println(name);
+    }
+
+    static void changeName(String name){
+        name = "rohan";
+    }
+
+    static void swap(int a,int b){
         int temp = a;
         a = b;
         b = temp;
-
-        System.out.println(a + " " + b);
     }
-
-
 }
