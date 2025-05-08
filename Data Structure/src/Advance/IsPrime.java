802c9c4c0c05f454954e46999992082fe363ded8
Rohan <rohanprasad1434@gmail.com>
2025-05-09 00:45:48 +0530

    commit method

---
+++ Data Structure/src/Advance/IsPrime.java
@@ -0,0 +1,27 @@
+package Advance;
+
+import java.util.Scanner;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+//        boolean ans = isPrime(n);
+//        System.out.println(ans);
+        System.out.println(isPrime(n));
+    }
+
+    static boolean isPrime(int n){
+        if (n <= 1){
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n){
+            if(n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        return c * c > n;
+    }
+}
