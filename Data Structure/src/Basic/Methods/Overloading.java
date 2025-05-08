b1e6122c5a52027c7a3b55cc6996e1193acb27ed
Rohan <rohanprasad1434@gmail.com>
2025-05-09 00:45:58 +0530

    commit method

---
+++ Data Structure/src/Basic/Methods/Overloading.java
@@ -0,0 +1,19 @@
+package Basic.Methods;
+
+public class Overloading {
+    public static void main(String[] args) {
+        fun(69);
+        fun("mansi");
+        fun(10,20);
+    }
+    static void fun(int a){
+        System.out.println(a);
+    }
+
+    static void fun(String name){
+        System.out.println(name);
+    }
+    static void fun(int a,int b){
+        System.out.println(a + b);
+    }
+}
