80c37b164511a957dae7372d8d644e20c877a4f6
Rohan <rohanprasad1434@gmail.com>
2025-05-08 22:57:57 +0530

    commit method

---
+++ Data Structure/src/Basic/Methods/PassingExample.java
@@ -0,0 +1,13 @@
+package Basic.Methods;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String name = "Rohan";
+        greet(name);
+    }
+
+    static void greet(String naam){
+        System.out.println(naam);
+
+    }
+}
