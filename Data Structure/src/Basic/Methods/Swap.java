47df8be7e44731635a5cfcce7feaf7e6b2e1b523
Rohan <rohanprasad1434@gmail.com>
2025-05-08 22:33:08 +0530

    commit method

---
+++ Data Structure/src/Basic/Methods/Swap.java
@@ -0,0 +1,17 @@
+package Basic.Methods;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        //swap numbers code
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+    }
+
+
+}
