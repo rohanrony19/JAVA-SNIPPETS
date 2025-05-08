59d856f90f60d80e33d6a7c8be38b40a245f7a3e
Rohan <rohanprasad1434@gmail.com>
2025-05-09 00:01:58 +0530

    commit method

--- Data Structure/src/Basic/Methods/ChangeValue.java
+++ Data Structure/src/Basic/Methods/ChangeValue.java
@@ -1,13 +1,13 @@
 package Basic.Methods;
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class ChangeValue {
     public static void main(String[] args) {
         // create an array
         int[] arr = {1,3,2,45,6};
         change(arr);
-        System.out.println(Array.toString(arr));
+        System.out.println(Arrays.toString(arr));
     }
 
     static void change(int[] nums){
