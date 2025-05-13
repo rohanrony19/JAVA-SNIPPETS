4328fb07735b747889f7156fdacbf6c1ded9b74e
Rohan <rohanprasad1434@gmail.com>
2025-05-13 23:26:34 +0530

    commit method

--- Data Structure/src/Basic/Methods/Greeting.java
+++ Data Structure/src/Basic/Methods/Greeting.java
@@ -2,7 +2,7 @@ package Basic.Methods;
 
 public class Greeting {
     public static void main(String[] args) {
-        greeting(); 
+        greeting();
     }
     static void greeting(){
         System.out.println("Hello World");
