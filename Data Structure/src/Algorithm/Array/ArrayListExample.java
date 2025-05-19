047b660f2d802e7a4eb3a12735ffa55bb6a389cd
Rohan <rohanprasad1434@gmail.com>
2025-05-19 20:35:11 +0530

    array update

--- Data Structure/src/Algorithm/Array/ArrayListExample.java
+++ Data Structure/src/Algorithm/Array/ArrayListExample.java
@@ -1,11 +1,39 @@
 package Algorithm.Array;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         // syntax
         //Array list in nothing which does not have size to be declare
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+//        list.add(67);
+//        list.add(68);
+//        list.add(69);
+//        list.add(64);
+//        list.add(557);
+//        list.add(688);
+//        list.add(70);
+//        list.add(72);
+
+//        System.out.println(list);
+//        list.set(0, 99);//0 index is 99
+//        list.remove(3);
+//        System.out.println(list);
+
+        //input
+        for(int i = 0 ; i < 5 ; i++){
+            list.add(in.nextInt());
+        }
+
+        //get item at any index
+        for(int i = 0 ; i < 5 ; i++){
+            System.out.println(list.get(i)); // pass index here,list[index] syntax will not work here
+        }
+        System.out.println();
+
     }
 }
---
+++ Data Structure/src/Algorithm/Array/Max.java
@@ -0,0 +1,28 @@
+package Algorithm.Array;
+
+public class Max {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 23, 54,18};
+        System.out.println(maxRange(arr,1,3));
+    }
+    static int maxRange(int[] arr, int start, int end){
+        int maxVal = arr[0];
+        for (int i = start; i <= end; i++) {
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    //imagine that arr is not empty
+    static int max(int[] arr){
+        int maxVal = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+}
