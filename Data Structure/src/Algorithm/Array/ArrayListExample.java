cc5a80af99308d1c69358d6f7f6c781869f477f5
Rohan <rohanprasad1434@gmail.com>
2025-05-19 19:39:41 +0530

    array update

---
+++ Data Structure/src/Algorithm/Array/ArrayListExample.java
@@ -0,0 +1,11 @@
+package Algorithm.Array;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        // syntax
+        //Array list in nothing which does not have size to be declare
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
