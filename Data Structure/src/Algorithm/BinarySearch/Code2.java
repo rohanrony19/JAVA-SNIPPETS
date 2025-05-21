6f9e1beb217ae0e16a946d7bca6bcf123b7745d7
Rohan <rohanprasad1434@gmail.com>
2025-05-22 01:18:54 +0530

    Algorithm update

--- Data Structure/src/Algorithm/BinarySearch/Code2.java
+++ Data Structure/src/Algorithm/BinarySearch/Code2.java
@@ -2,7 +2,7 @@ package Algorithm.BinarySearch;
 
 public class Code2 {
     public static void main(String[] args) {
-        int[] arr = { -18,-4,0,2,3,4,5,15,33,58,72,89,92};
+        int[] arr = {-18,-4,0,2,3,4,5,15,33,58,72,89,92};
         int target = 72;
         int ans = binarySearch(arr,target);
         System.out.println(ans);
@@ -14,7 +14,7 @@ public class Code2 {
         int end = arr.length - 1;
         while (start <= end){
             // Find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + ( end - start) / 2;
             // arr[mid] = element
             // mid = index
