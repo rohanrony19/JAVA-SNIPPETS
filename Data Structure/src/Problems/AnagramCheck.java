b3f6c5cec0561b1f932ec2351923f83e7aea3ac6
Rohan <rohanprasad1434@gmail.com>
2025-05-23 00:23:06 +0530

    Algorithm update

---
+++ Data Structure/src/Problems/AnagramCheck.java
@@ -0,0 +1,18 @@
+package Problems;
+
+import java.util.Arrays;
+
+// Check if Two Strings Are Anagrams
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        System.out.println(Arrays.equals(a, b) ? "Anagram" : "Not Anagram");
+    }
+}
