b2fbc324f4dd39fb650235aae0ee2d3bb7d684b8
Rohan <rohanprasad1434@gmail.com>
2025-06-17 22:01:16 +0530

    commit jdbc

---
+++ JDBC/src/com/xworkz/jdbc/dto/InfoDTO.java
@@ -0,0 +1,74 @@
+package com.xworkz.jdbc.dto;
+
+import java.io.Serializable;
+
+public class InfoDTO implements Serializable {
+
+    private String name;
+    private String fatherName;
+    private String motherName;
+    private String email;
+    private long phoneNumber;
+
+    public InfoDTO(String name, String fatherName, String motherName, String email, long phoneNumber) {
+        this.name = name;
+        this.fatherName = fatherName;
+        this.motherName = motherName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public InfoDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "InfoDTO{" +
+                "name='" + name + '\'' +
+                ", fatherName='" + fatherName + '\'' +
+                ", motherName='" + motherName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
