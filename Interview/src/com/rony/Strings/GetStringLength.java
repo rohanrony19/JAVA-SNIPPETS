b6fe58dd40fe402d7c2155c54998486169817f4a
Rohan <rohanprasad1434@gmail.com>
2025-06-20 22:44:27 +0530

    prepare

---
+++ Interview/src/com/rony/Strings/GetStringLength.java
@@ -0,0 +1,9 @@
+package com.rony.Strings;
+
+public class GetStringLength {
+    public static void main(String[] args) {
+        String name = "Rohan";
+        int length = name.length();
+        System.out.println("The length of " +name+" is: " + length);
+    }
+}
