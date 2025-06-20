3f661acce90b171a998bd905f878406d20f6e449
Rohan <rohanprasad1434@gmail.com>
2025-06-20 22:44:34 +0530

    prepare

---
+++ Interview/src/com/rony/Strings/Factorial.java
@@ -0,0 +1,12 @@
+package com.rony.Strings;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 10;
+        int fact = 1;
+        for (int i = 1; i <= n ; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial = " + fact);
+    }
+}
