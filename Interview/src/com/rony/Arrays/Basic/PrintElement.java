18de5719ac259cbccb92a3adb078e3cb683b3d19
Rohan <rohanprasad1434@gmail.com>
2025-06-23 19:50:10 +0530

    prepare

---
+++ Interview/src/com/rony/Arrays/Basic/PrintElement.java
@@ -0,0 +1,10 @@
+package com.rony.Arrays.Basic;
+
+import java.util.Arrays;
+
+public class PrintElement {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.print(Arrays.toString(arr));
+    }
+}
