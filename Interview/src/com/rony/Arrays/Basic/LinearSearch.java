9af4e46308882c8e3b9350577c3fc9284e69523f
Rohan <rohanprasad1434@gmail.com>
2025-06-25 21:32:16 +0530

    prepare

---
+++ Interview/src/com/rony/Arrays/Basic/LinearSearch.java
@@ -0,0 +1,19 @@
+package com.rony.Arrays.Basic;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,6,2,7,9};
+        int target = 6;
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                found = true;
+                System.out.println("Target in index = " + i);
+                break;
+            }
+        }
+            if(!found)
+                System.out.println("target Not found");
+            }
+        }
+
