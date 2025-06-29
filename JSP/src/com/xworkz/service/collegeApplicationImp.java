41641d0c196e743eb965978d8855bc4563c86f9c
Rohan <rohanprasad1434@gmail.com>
2025-06-30 00:36:47 +0530

    commit service

---
+++ JSP/src/com/xworkz/service/collegeApplicationImp.java
@@ -0,0 +1,22 @@
+package com.xworkz.service;
+
+import com.xworkz.Repository.collegeRepository;
+import com.xworkz.Repository.collegeRepositoryImpl;
+import com.xworkz.dto.collegedto;
+
+public class collegeApplicationImp implements collegeApplicationService{
+
+    @Override
+    public String collegeApplicationValidated(collegedto college) {
+        System.out.println("-------------");
+        if (college != null) {
+            System.out.println("data is valid");
+            collegeRepository collegerepo = new collegeRepositoryImpl();
+            collegerepo.save(college);
+            return "success";
+        } else {
+            System.out.println("validation failed");
+            return "failed";
+        }
+    }
+}
