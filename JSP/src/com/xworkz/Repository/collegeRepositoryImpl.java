e461967cba70fb3851526846d86cdee652809bab
Rohan <rohanprasad1434@gmail.com>
2025-07-01 21:51:43 +0530

    commit service

--- JSP/src/com/xworkz/Repository/collegeRepositoryImpl.java
+++ JSP/src/com/xworkz/Repository/collegeRepositoryImpl.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-     
+        
 public class collegeRepositoryImpl implements collegeRepository{
     @Override
     public void save(collegedto dto) {
