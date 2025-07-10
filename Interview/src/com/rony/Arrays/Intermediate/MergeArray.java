27f80bff6a2e9ff258bf6c83d982a918ce01b7d7
Rohan <rohanprasad1434@gmail.com>
2025-07-10 22:50:32 +0530

    prepare

--- Interview/src/com/rony/Arrays/Intermediate/MergeArray.java
+++ Interview/src/com/rony/Arrays/Intermediate/MergeArray.java
@@ -10,7 +10,7 @@ public class MergeArray {
         int[] merged = new int[n1 + n2];
 
         int i = 0, j = 0, k = 0;
-
+  
         // Merge arrays
         while (i < n1 && j < n2) {
             if (arr1[i] < arr2[j]) {
