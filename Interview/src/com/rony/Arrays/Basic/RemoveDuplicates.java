c55ddcad0a4ac7a21ce1897dcc69b29ea4f3fa8b
Rohan <rohanprasad1434@gmail.com>
2025-07-12 23:52:39 +0500

    prepare

--- Interview/src/com/rony/Arrays/Basic/RemoveDuplicates.java
+++ Interview/src/com/rony/Arrays/Basic/RemoveDuplicates.java
@@ -10,7 +10,7 @@ public class RemoveDuplicates {
 
         for (int i = 0; i < n - 1; i++) {
             if (arr[i] != arr[i + 1]) {
-                temp[j++] = arr[i];  
+                temp[j++] = arr[i];
             }
         }
         temp[j++] = arr[n - 1]; // last element
