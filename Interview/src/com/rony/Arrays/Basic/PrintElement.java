0e55aea1b375dfe78cd82442b9cd841d24af920c
Rohan <rohanprasad1434@gmail.com>
2025-07-13 19:03:00 +0530

    prepare

--- Interview/src/com/rony/Arrays/Basic/PrintElement.java
+++ Interview/src/com/rony/Arrays/Basic/PrintElement.java
@@ -3,7 +3,7 @@ package com.rony.Arrays.Basic;
 import java.util.Arrays;
 
 public class PrintElement {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         int[] arr = {1,2,3,4,5};
         System.out.print(Arrays.toString(arr));
     }
