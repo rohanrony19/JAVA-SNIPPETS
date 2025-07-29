4bdfbdd671d4329c09bef52c5bf058d5831f18b2
Rohan <rohanprasad1434@gmail.com>
2025-07-29 22:42:00 +0530

    spring

---
+++ Spring/src/main/java/com/xworkz/manage/configuration/WebInit.java
@@ -0,0 +1,24 @@
+package com.xworkz.manage.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ManageConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+}
+
+
