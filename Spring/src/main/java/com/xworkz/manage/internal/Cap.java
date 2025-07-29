9c2a90e92f83e125abbae1b4ae62088c446aaf26
Rohan <rohanprasad1434@gmail.com>
2025-07-29 22:41:33 +0530

    spring

---
+++ Spring/src/main/java/com/xworkz/manage/internal/Cap.java
@@ -0,0 +1,26 @@
+package com.xworkz.manage.internal;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cap {
+
+    public Cap(){
+        System.out.println("running cap by spring");
+    }
+
+    @Autowired
+    @Qualifier("Tiffen")
+    private String Tiffen;
+
+    @Autowired
+    public String Metal;
+
+    @Autowired
+    public Box box;
+
+
+}
