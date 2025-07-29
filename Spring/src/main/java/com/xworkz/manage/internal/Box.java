b4c48b360c1580963b73afc0d981837134463f9d
Rohan <rohanprasad1434@gmail.com>
2025-07-29 22:41:28 +0530

    spring

---
+++ Spring/src/main/java/com/xworkz/manage/internal/Box.java
@@ -0,0 +1,20 @@
+package com.xworkz.manage.internal;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Box {
+
+
+    private String Tiffen;
+
+    private String Metal;
+
+    @Bean
+    public Box(String Tiffen,String Metal){
+        this.Tiffen = Tiffen;
+        this.Metal=Metal;
+        System.out.println("Box Constructor");
+    }
+}
