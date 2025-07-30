991bdc55bee60da8cba22c8ea5764c8b3ce678d4
Rohan <rohanprasad1434@gmail.com>
2025-07-30 21:54:56 +0530

    commit jpa

---
+++ JPA/src/main/java/com/xworkz/Football/Runner/FootballRunner.java
@@ -0,0 +1,20 @@
+package com.xworkz.Football.Runner;
+
+import com.xworkz.Football.Entity.FootballEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class FootballRunner {
+    public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("football");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        FootballEntity footballEntity = new FootballEntity(1,"Real Madrid","Spain");
+        em.persist(footballEntity);
+        et.commit();
+    }
+}
