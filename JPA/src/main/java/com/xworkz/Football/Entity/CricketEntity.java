76dc64dc0a76053d2bf4e1ea422cc24b7b9dd1aa
Rohan <rohanprasad1434@gmail.com>
2025-07-31 23:04:39 +0530

    commit jpa

--- JPA/src/main/java/com/xworkz/Football/Entity/CricketEntity.java
+++ JPA/src/main/java/com/xworkz/Football/Entity/CricketEntity.java
@@ -11,14 +11,87 @@ public class CricketEntity {
     @Id
     private int id;
 
-    @Column(name="")
+    @Column(name="teamName")
     private String teamName;
 
+    @Column(name = "country")
     private String country;
 
+    @Column(name = "NumberofPlayers")
     private int noOfPlayers;
 
+    @Column(name = "CaptainName")
     private String captainName;
 
+    @Column(name = "MatchesWon")
     private int matchesWon;
+
+    public CricketEntity(int id, String teamName, String country, int noOfPlayers, String captainName, int matchesWon) {
+        this.id = id;
+        this.teamName = teamName;
+        this.country = country;
+        this.noOfPlayers = noOfPlayers;
+        this.captainName = captainName;
+        this.matchesWon = matchesWon;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getNoOfPlayers() {
+        return noOfPlayers;
+    }
+
+    public void setNoOfPlayers(int noOfPlayers) {
+        this.noOfPlayers = noOfPlayers;
+    }
+
+    public String getCaptainName() {
+        return captainName;
+    }
+
+    public void setCaptainName(String captainName) {
+        this.captainName = captainName;
+    }
+
+    public int getMatchesWon() {
+        return matchesWon;
+    }
+
+    public void setMatchesWon(int matchesWon) {
+        this.matchesWon = matchesWon;
+    }
+
+    @Override
+    public String toString() {
+        return "CricketEntity{" +
+                "id=" + id +
+                ", teamName='" + teamName + '\'' +
+                ", country='" + country + '\'' +
+                ", noOfPlayers=" + noOfPlayers +
+                ", captainName='" + captainName + '\'' +
+                ", matchesWon=" + matchesWon +
+                '}';
+    }
 }
