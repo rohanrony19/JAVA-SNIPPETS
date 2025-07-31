65dda0f0576c0929d4a9fd2192eaf08c01ed87ab
Rohan <rohanprasad1434@gmail.com>
2025-07-31 22:59:51 +0530

    commit jpa

---
+++ JPA/src/main/java/com/xworkz/Football/Entity/CricketEntity.java
@@ -0,0 +1,22 @@
+package com.xworkz.Football.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "")
+public class CricketEntity {
+    @Id
+    private int id;
+
+    private String teamName;
+
+    private String country;
+
+    private int noOfPlayers;
+
+    private String captainName;
+
+    private int matchesWon;
+}
