f4046092198f2db67032e4454c0b41cf2ed62fdb
Rohan <rohanprasad1434@gmail.com>
2025-07-31 23:00:06 +0530

    commit jpa

--- JPA/src/main/java/com/xworkz/Football/Runner/FootballRunner.java
+++ JPA/src/main/java/com/xworkz/Football/Runner/FootballRunner.java
@@ -6,15 +6,33 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FootballRunner {
     public static void main(String[] args) {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("football");
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernate");
         EntityManager em = emf.createEntityManager();
         EntityTransaction et = em.getTransaction();
-        et.begin(); 
-        FootballEntity footballEntity = new FootballEntity(1,"Real Madrid","Spain");
-        em.persist(footballEntity);
+        et.begin();
+//        FootballEntity footballEntity = new FootballEntity(1,"Real Madrid","Spain",11,"Dani Carvajal",101);
+
+        List<FootballEntity> list = new ArrayList<>();
+        list.add(new FootballEntity(1,"Real Madrid","Spain",11,"Dani Carvajal",101));
+        list.add(new FootballEntity(2,"Manchester United", "England", 11, "Bruno Fernandes", 25));
+        list.add(new FootballEntity(3,"Ajax", "Netherlands", 11, "Steven Bergwijn", 21));
+        list.add(new FootballEntity(4,"Bayern Munich", "Germany", 11, "Manuel Neuer", 28));
+        list.add(new FootballEntity(5,"Juventus", "Italy", 11, "Danilo", 24));
+        list.add(new FootballEntity(6,"Paris Saint-Germain", "France", 11, "Marquinhos", 27));
+        list.add(new FootballEntity(7,"Barcelona", "Spain", 11, "Robert Lewandowski", 26));
+        list.add(new FootballEntity(8,"Chelsea", "England", 11, "Reece James", 20));
+        list.add(new FootballEntity(9,"AC Milan", "Italy", 11, "Davide Calabria", 22));
+        list.add(new FootballEntity(10,"Borussia Dortmund", "Germany", 11, "Marco Reus", 23));
+        for(FootballEntity entity:list){
+            em.persist(entity);
+        }
+
+
         et.commit();
     }
 }
