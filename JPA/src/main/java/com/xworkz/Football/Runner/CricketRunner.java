be9be0d0867c2dea82e71994526e4065040ceb18
Rohan <rohanprasad1434@gmail.com>
2025-07-31 22:59:56 +0530

    commit jpa

---
+++ JPA/src/main/java/com/xworkz/Football/Runner/CricketRunner.java
@@ -0,0 +1,24 @@
+package com.xworkz.Football.Runner;
+
+import com.xworkz.Football.Entity.CricketEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CricketRunner {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernate");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+//        List<CricketEntity> list = new ArrayList<>();
+//        for (CricketEntity entity:list){
+//            em.persist();
+//        }
+//        et.commit();
+    }
+}
