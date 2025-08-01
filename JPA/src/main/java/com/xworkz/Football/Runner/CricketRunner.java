40fe49a8f5359746b028f0d6670bd86738046e62
Rohan <rohanprasad1434@gmail.com>
2025-08-01 22:25:33 +0530

    commit jpa

--- JPA/src/main/java/com/xworkz/Football/Runner/CricketRunner.java
+++ JPA/src/main/java/com/xworkz/Football/Runner/CricketRunner.java
@@ -15,12 +15,23 @@ public class CricketRunner {
         EntityManager em = emf.createEntityManager();
         EntityTransaction et = em.getTransaction();
         et.begin();
-        List<CricketEntity> list = new ArrayList<>();
-        list.add(new CricketEntity());
-        list
-        for (CricketEntity entity:list){
+        List<CricketEntity> cricketList = new ArrayList<>();
+        cricketList.add(new CricketEntity(1, "Mumbai Indians", "India", 11, "Rohit Sharma", 130));
+        cricketList.add(new CricketEntity(2, "Sydney Sixers", "Australia", 11, "Moises Henriques", 95));
+        cricketList.add(new CricketEntity(3, "Chennai Super Kings", "India", 11, "MS Dhoni", 125));
+        cricketList.add(new CricketEntity(4, "Melbourne Stars", "Australia", 11, "Glenn Maxwell", 85));
+        cricketList.add(new CricketEntity(5, "Karachi Kings", "Pakistan", 11, "Imad Wasim", 72));
+        cricketList.add(new CricketEntity(6, "Lahore Qalandars", "Pakistan", 11, "Shaheen Afridi", 68));
+        cricketList.add(new CricketEntity(7, "Oval Invincibles", "England", 11, "Sam Billings", 47));
+        cricketList.add(new CricketEntity(8, "Trinbago Knight Riders", "West Indies", 11, "Kieron Pollard", 103));
+        cricketList.add(new CricketEntity(9, "Pretoria Capitals", "South Africa", 11, "Wayne Parnell", 36));
+        cricketList.add(new CricketEntity(10, "Colombo Kings", "Sri Lanka", 11, "Angelo Mathews", 64));
+        for (CricketEntity entity:cricketList){
             em.persist(entity);
         }
         et.commit();
+
+        CricketEntity cricketEntity = em.find(CricketEntity.class,1);
+        System.out.println(cricketEntity);
     }
 }
