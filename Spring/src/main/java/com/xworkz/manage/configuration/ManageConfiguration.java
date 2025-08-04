57cac04e262aa8d3b373772e8749e461343727b3
Rohan <rohanprasad1434@gmail.com>
2025-08-04 22:52:57 +0530

    spring

--- Spring/src/main/java/com/xworkz/manage/configuration/ManageConfiguration.java
+++ Spring/src/main/java/com/xworkz/manage/configuration/ManageConfiguration.java
@@ -16,6 +16,7 @@ public class ManageConfiguration  {
     public ManageConfiguration(){
         System.out.println("running manageConfiguration");
     }
+ 
     @Bean//custom class (.java/.class)
     String getMail(){
 
