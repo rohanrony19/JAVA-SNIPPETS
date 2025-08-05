2e1a6170871634558872434126f866ed8517c1a2
Rohan <rohanprasad1434@gmail.com>
2025-08-05 23:50:41 +0530

    spring

--- Spring/src/main/java/com/xworkz/manage/configuration/WebInit.java
+++ Spring/src/main/java/com/xworkz/manage/configuration/WebInit.java
@@ -4,12 +4,12 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-  
+
     @Override
     protected String[] getServletMappings() {
         return new String[0];
     }
-
+  
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{ManageConfiguration.class};
