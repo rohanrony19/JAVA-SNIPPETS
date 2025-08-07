ecceb0e1fc1f022a2833e6c3c65f84e364624383
Rohan <rohanprasad1434@gmail.com>
2025-08-07 23:01:08 +0530

    spring container

---
+++ snake/src/main/java/com/xworkz/snake/controller/color.java
@@ -0,0 +1,12 @@
+package com.xworkz.snake.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class color {
+    public color(){
+        System.out.println("in controller color");
+    }
+
+
+}
