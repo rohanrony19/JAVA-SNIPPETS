bdfdf70c2b731b4df0112a1eb3ab34973256e661
Rohan <rohanprasad1434@gmail.com>
2025-08-13 22:59:15 +0530

    jpa2

---
+++ JPA2/src/main/java/com/xworkz/Vehicle/Service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package com.xworkz.Vehicle.Service;
+
+import com.xworkz.Vehicle.Entity.CarEntity;
+import com.xworkz.Vehicle.Repository.CarRepository;
+import com.xworkz.Vehicle.Repository.CarRepositoryImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarServiceImpl implements CarService{
+    @Override
+    public String save(CarEntity carEntity) {
+        System.out.println("Running service");
+        return new CarServiceImpl().save(carEntity);
+    }
+
+    @Override
+    public CarEntity getEntityById(int id) {
+        System.out.println("getEntityById");
+        return new CarRepositoryImpl().FindById(2);
+    }
+
+//    @Override
+//    public CarEntity getEntityByName(String Name) {
+//
+//        return new CarRepositoryImpl().UpdateById("Porche");
+//    }
+}
+}
