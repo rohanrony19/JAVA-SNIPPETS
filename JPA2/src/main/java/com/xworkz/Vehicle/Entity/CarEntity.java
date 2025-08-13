e0ee8b97214959687150f7bd674007ecae85ad22
Rohan <rohanprasad1434@gmail.com>
2025-08-13 22:58:49 +0530

    jpa2

---
+++ JPA2/src/main/java/com/xworkz/Vehicle/Entity/CarEntity.java
@@ -0,0 +1,53 @@
+package com.xworkz.Vehicle.Entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cars")
+@NamedQuery(name="EntityByName",query = "select entity from CarEntity entity where entity.name =:Name ")
+@NamedQuery(name="EntityByNameAndType",query = "select entity from CarEntity entity where entity.name =:name and entity.type =: type ")
+@NamedQuery(name="EntityByNameAndColor",query = "select entity from CarEntity entity where entity.name =:name and entity.color=:color ")
+@NamedQuery(name="EntityByPrice",query = "select entity from CarEntity entity where entity.price =: price ")
+
+public class CarEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "Car_Name")
+    private String carName;
+
+    @Column(name = "Car_Color")
+    private String carColor;
+
+    @Column(name = "Car_Price")
+    private int carPrice;
+
+    @Column(name = "Car_Type")
+    private String carType;
+
+    public CarEntity(String carName, String carColor, int carPrice, String carType) {
+        this.carName = carName;
+        this.carColor = carColor;
+        this.carPrice = carPrice;
+        this.carType = carType;
+    }
+
+    @Override
+    public String toString() {
+        return "CarEntity{" +
+                "id=" + id +
+                ", carName='" + carName + '\'' +
+                ", carColor='" + carColor + '\'' +
+                ", carPrice=" + carPrice +
+                ", carType='" + carType + '\'' +
+                '}';
+    }
+}
