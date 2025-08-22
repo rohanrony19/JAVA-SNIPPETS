99f63cb6a0d4b13c880d5ec6fe83f32c86b12b7a
Rohan <rohanprasad1434@gmail.com>
2025-08-22 23:11:01 +0530

    Restful API

---
+++ SpringBootRestAPI/src/test/java/com/xworkz/restapi/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xworkz.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
