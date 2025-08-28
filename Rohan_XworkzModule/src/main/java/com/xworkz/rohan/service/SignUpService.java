40f9e468571cf79c0301ed7b1acb4f12f5445530
Rohan <rohanprasad1434@gmail.com>
2025-08-28 22:43:21 +0530

    jpa + spring

--- Rohan_XworkzModule/src/main/java/com/xworkz/rohan/service/SignUpService.java
+++ Rohan_XworkzModule/src/main/java/com/xworkz/rohan/service/SignUpService.java
@@ -3,7 +3,6 @@ package com.xworkz.rohan.service;
 import com.xworkz.rohan.dto.SignUpDTO;
 import org.springframework.stereotype.Component;
 
-
 public interface SignUpService {
     String  validate(SignUpDTO sign);
 }
