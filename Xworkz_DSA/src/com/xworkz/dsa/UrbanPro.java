9810fb0d010b85bf0e7b71cc8977b211746a8dfd
Rohan <rohanprasad1434@gmail.com>
2025-09-10 22:22:21 +0530

    DSA

--- Xworkz_DSA/src/com/xworkz/dsa/UrbanPro.java
+++ Xworkz_DSA/src/com/xworkz/dsa/UrbanPro.java
@@ -21,7 +21,7 @@ public class UrbanPro {
 //        Set<Integer> duplicates =
 //                list.stream().filter(i-> Collections.frequency(list,i)>1).collect(Collectors.toSet());
 //        System.out.println("Duplicate elements: " + duplicates);
-
+ 
 //        O(n)
         int[] arr= {1, 3, 4, 2, 3, 6, 4, 5 ,4 ,2};
         Map<Integer,Integer> freq = new HashMap<>();
