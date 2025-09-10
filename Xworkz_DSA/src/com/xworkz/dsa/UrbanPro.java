416e543db354590923e05bf2f4e8e6cdc2a62585
Rohan <rohanprasad1434@gmail.com>
2025-09-10 22:20:30 +0530

    DSA

--- Xworkz_DSA/src/com/xworkz/dsa/UrbanPro.java
+++ Xworkz_DSA/src/com/xworkz/dsa/UrbanPro.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class UrbanPro {
     public static void main(String[] args) {
-//        int[] arr= {1, 3, 4, 2, 3, 6, 4, 5};
+//        int[] arr= {1, 3, 4, 2, 3, 6, 4, 5}; O(n^2)
 //        System.out.println("Duplicate elements in the array:");
 //        for (int i = 0; i < arr.length; i++) {
 //            for (int j = i+1; j < arr.length; j++) {
@@ -20,7 +20,7 @@ public class UrbanPro {
 //        Set<Integer> duplicates =
 //                list.stream().filter(i-> Collections.frequency(list,i)>1).collect(Collectors.toSet());
 //        System.out.println("Duplicate elements: " + duplicates);
-        
+
         int[] arr= {1, 3, 4, 2, 3, 6, 4, 5,4,2};
         Map<Integer,Integer> freq = new HashMap<>();
         Set<Integer> duplicates = new HashSet<>();
