b607b0514e54ffe3bb1467bc8474f415a0dcfc9c
Rohan <rohanprasad1434@gmail.com>
2025-09-24 21:57:12 +0530

    DSA

--- Xworkz_DSA/src/com/xworkz/dsa/SumOfEvenOdd.java
+++ Xworkz_DSA/src/com/xworkz/dsa/SumOfEvenOdd.java
@@ -16,7 +16,6 @@ public class SumOfEvenOdd {
                 odd += i;
             }
         }
-
         System.out.println("Sum of Even Numbers from 0 to " + sum + " is: " + even);
         System.out.println("Sum of Odd Numbers from 0 to " + sum + " is: " + odd);
     }
